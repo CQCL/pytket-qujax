6427c5edd5c677b15443ad82e728b28790ac067d
Sam Duffield <s@mduffield.com>
2022-07-25 14:59:20 +0100

    test QubitPauliOperator list

--- tests/test_tket.py
+++ tests/test_tket.py
@@ -226,10 +226,10 @@ def test_quantum_hamiltonian() -> None:
         for j in range(n_qubits - 1)
     ]
     coefs_zz = random.normal(random.PRNGKey(0), shape=(len(strings_zz),))
-    tket_op_dict_zz = dict(zip(strings_zz, coefs_zz))
+    tket_op_dict_zz = dict(zip(strings_zz, coefs_zz.tolist()))
     strings_x = [QubitPauliString({Qubit(j): Pauli.X}) for j in range(n_qubits)]
     coefs_x = random.normal(random.PRNGKey(0), shape=(len(strings_x),))
-    tket_op_dict_x = dict(zip(strings_x, coefs_x))
+    tket_op_dict_x = dict(zip(strings_x, coefs_x.tolist()))
     tket_op = QubitPauliOperator({**tket_op_dict_zz, **tket_op_dict_x})
 
     gate_str_seq_seq = [["Z", "Z"]] * (n_qubits - 1) + [["X"]] * n_qubits
