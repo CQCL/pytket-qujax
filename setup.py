158e7fa0818fc57c09868d35df2c80680a7f2a74
Sam Duffield <s@mduffield.com>
2022-08-18 10:06:40 +0100

    clean doc strings + qujax version bump

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "pytket ~= 1.5",
-        "qujax ~= 0.1.5",
+        "qujax ~= 0.2.0",
     ],
     classifiers=[
         "Environment :: Console",
