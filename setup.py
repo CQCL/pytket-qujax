69d9c95b05e6f01032b2977b7afa989a4c95554e
Sam Duffield <s@mduffield.com>
2022-08-18 12:04:59 +0100

    qujax 0.2.3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "pytket ~= 1.5",
-        "qujax ~= 0.2.2",
+        "qujax ~= 0.2.3",
     ],
     classifiers=[
         "Environment :: Console",
