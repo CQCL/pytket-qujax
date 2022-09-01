f4a1fae3c7199b3b1ab78b0b1dc6b29c3e2e6992
kirill <kirill.plekhanov@cambridgequantum.com>
2022-09-01 17:06:01 +0100

    fixed tests for qujax_args_to_tk

--- tests/test_tket_symbolic.py
+++ tests/test_tket_symbolic.py
@@ -21,7 +21,7 @@ from pytket.circuit import Circuit
 from pytket.extensions.qujax import (
     tk_to_qujax,
     tk_to_qujax_args,
-    qujax_to_tk,
+    qujax_args_to_tk,
 )
 
 
@@ -62,7 +62,7 @@ def _test_circuit(
         circuit_commands = [
             com for com in circuit.get_commands() if str(com.op) != "Barrier"
         ]
-        circuit_2 = qujax_to_tk(*tk_to_qujax_args(circuit, symbol_map))
+        circuit_2 = qujax_args_to_tk(*tk_to_qujax_args(circuit, symbol_map))
         assert all(
             g.op.type == g2.op.type
             for g, g2 in zip(circuit_commands, circuit_2.get_commands())
