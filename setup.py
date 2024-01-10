5cfef3e445cd0bde78ac792bb6c259cb325a6314
Melf <cqc@melf.de>
2024-01-10 14:52:59 +0000

    update pytket version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.22",
+        "pytket ~= 1.23",
         "qujax ~= 1.0",
     ],
     classifiers=[
